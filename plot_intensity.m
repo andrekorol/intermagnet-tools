bdb12acf25c6eed1a783c99c82fc7bfe6d542d20
andrekorol <anrobits@yahoo.com.br>
2019-05-02 16:24:05 -0300

    Read data files and get values into cell arrays

--- plot_intensity.m
+++ plot_intensity.m
@@ -1,11 +1,54 @@
 unzip(fullfile('data', 'data20190417232148.zip'), 'data');
-data = dir(fullfile('data', '*.gz'));
-fields = fieldnames(data);
-for i = 1:numel(fieldnames(data))
-    disp(data(i).name);
-end
+
 files = [dir(fullfile('data', '*.gz')); dir(fullfile('data', '*.txt'))];
 files = {files.name};
+
+num_of_files = 0;
+for i = 1:numel(files)
+    file = strcat('data', filesep, files(i));
+    [filepath,name,ext] = fileparts(file{1});
+    if strcmp(ext, '.gz')
+        num_of_files = num_of_files + 1;
+    end
+end
+
+array_size = num_of_files  * 1440;
+
+honx_array = cell(1, array_size);
+hony_array = cell(1, array_size);
+honz_array = cell(1, array_size);
+hong_array = cell(1, array_size);
+
+cell_pos = 1;
+
+for i = 1:numel(files)
+    file = strcat('data', filesep, files(i));
+    [filepath,name,ext] = fileparts(file{1});
+    if strcmp(ext, '.gz')
+        extracted_files = gunzip(file);
+        fileID = fopen(fullfile(filepath, name), 'r');
+        while ~feof(fileID)
+            tline = fgetl(fileID);
+            if ~strcmp(tline(1), ' ') && ~strcmp(tline(1), 'D')
+                values = split(tline);
+                honx = values(4);
+                hony = values(5);
+                honz = values(6);
+                hong = values(7);
+                
+                honx_array(1, cell_pos) = honx;
+                hony_array(1, cell_pos) = hony;
+                honz_array(1, cell_pos) = honz;
+                hong_array(1, cell_pos) = hong;
+                
+                cell_pos = cell_pos + 1;
+            end
+        end
+        fclose(fileID);
+        delete(extracted_files{1});
+    end
+end
+
 for k = 1:numel(files)
     delete(['data' filesep files{k}]);
 end
